9be5e02521decbbb629a506152be152500a3790f
GiungKim <forever4191@naver.com>
2021-12-03 16:16:36 +0900

    Update version.h

--- port/version.h
+++ port/version.h
@@ -1,9 +1,20 @@
 /**
+ * Copyright (c) 2021 WIZnet Co.,Ltd
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+ 
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+/**
   * ----------------------------------------------------------------------------------------------------
-  * Version
+  * Macros
   * ----------------------------------------------------------------------------------------------------
   */
 
 #define MAJOR_VERSION    2
 #define MINOR_VERSION    0
 #define PATCH_VERSION    0
+
+#endif /* _VERSION_H_ */
