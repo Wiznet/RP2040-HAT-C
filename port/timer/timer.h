f9d4fa65ef92ecd78dbd37fe03f5794db0748f67
hyoyun-Kim <hyo-yun1103@daum.net>
2021-12-03 13:41:37 +0900

    Add functions information
    Add functions information in w5x00_spi.h / timer.h

--- port/timer/timer.h
+++ port/timer/timer.h
@@ -12,19 +12,33 @@
   * Macros
   * ----------------------------------------------------------------------------------------------------
   */
-/* Timeout */
-#define RECV_TIMEOUT (1000 * 10) // 10 seconds
-
-/**
-  * ----------------------------------------------------------------------------------------------------
-  * Functions
-  * ----------------------------------------------------------------------------------------------------
-  */
 /* Timer */
+/*! \brief Initialize timer callback function
+ * \ingroup timer
+ *
+ * Add a repeating timer that is called repeatedly at the specified interval in microseconds.
+ *
+ * \param callback the repeating timer callback function
+ */
 void wizchip_1ms_timer_initialize(void (*callback)(void));
+
+/*! \brief Assign timer callback function
+ * \ingroup timer
+ *
+ * 1ms timer callback function.
+ * 
+ * \param t Information about a repeating timer
+ */
 bool wizchip_1ms_timer_callback(struct repeating_timer *t);
 
 /* Delay */
+/*! \brief Wait for the given number of milliseconds before returning
+ * \ingroup timer
+ *
+ * This method attempts to perform a lower power sleep (using WFE) as much as possible.
+ *
+ * \param ms the number of milliseconds to sleep
+ */
 void wizchip_delay_ms(uint32_t ms);
 
 #endif /* _TIMER_H_ */
